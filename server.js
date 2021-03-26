7d0d342206c0090ae7fc5f1beec81a9ce293ddd5
Lajos <kossuthl4jos@gmail.com>
2021-03-26 18:05:30 +0100

    connect to db

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
 
 const app = express();
 
@@ -7,8 +7,31 @@ const port = 3001;
 
 app.use(express.urlencoded({ extended: true}));
 
-require('./app/routes')(app, {});
+const MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+const credentials = fs.readFileSync(process.env.MONGODB_CERTIFICATE_PATH);
+const client = new MongoClient(process.env.MONGODB_URI, {
+  sslKey: credentials,
+  sslCert: credentials,
+  useUnifiedTopology: true
+});
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("testDB");
+    const collection = database.collection("testCol");
 
-app.listen(port, function() {
-  console.log('Listening on', port);
-})
+    require('./app/routes')(app, database);
+    app.listen(port, function() {
+      console.log('Listening on', port);
+    })
+
+    const docCount = await collection.countDocuments({});
+    console.log(docCount);
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
