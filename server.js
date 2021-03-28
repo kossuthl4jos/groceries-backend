242d10b0bdb0eb0e1054f6e6a3fbd63dfaa8d122
Lajos <kossuthl4jos@gmail.com>
2021-03-28 20:36:39 +0200

    add cors for localhost

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var cors = require('cors')
 require('dotenv').config();
 
 const app = express();
@@ -6,6 +7,7 @@ const app = express();
 const port = 3001;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({origin: 'http://localhost:3000'}));
 
 const MongoClient = require('mongodb').MongoClient;
 const fs = require('fs');
