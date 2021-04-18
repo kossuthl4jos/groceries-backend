b4efb136da19ec1fb82be40ffe8605553b318660
Lajos <kossuthl4jos@gmail.com>
2021-04-18 19:05:49 +0200

    improve imports

--- app/data-base/db-api.js
+++ app/data-base/db-api.js
@@ -1,40 +1,38 @@
-var ObjectID = require('mongodb').ObjectID;
+import { ObjectID } from 'mongodb';
 const LISTS_COLLECTION = 'Lists';
 const USERS_COLLECTION = 'Users';
 
-module.exports = {
-  addList: async function (db, list) {
-    const query = await db.collection(LISTS_COLLECTION).insertOne(list);
-    return query.ops[0];
-  },
-  getList: async function (db, id) {
-    const deatils = { _id: new ObjectID(id) };
+export async function addList(db, list) {
+  const query = await db.collection(LISTS_COLLECTION).insertOne(list);
+  return query.ops[0];
+}
+export async function getList(db, id) {
+  const deatils = { _id: new ObjectID(id) };
 
-    const query = await db.collection(LISTS_COLLECTION).findOne(deatils);
-    return query;
-  },
-  getAllLists: async function (db) {
-    const query = await db.collection(LISTS_COLLECTION).find({});
-    return query.toArray();
-  },
-  updateList: async function (db, id, list) {
-    const deatils = { _id: new ObjectID(id) };
+  const query = await db.collection(LISTS_COLLECTION).findOne(deatils);
+  return query;
+}
+export async function getAllLists(db) {
+  const query = await db.collection(LISTS_COLLECTION).find({});
+  return query.toArray();
+}
+export async function updateList(db, id, list) {
+  const deatils = { _id: new ObjectID(id) };
 
-    const query = await db.collection(LISTS_COLLECTION).updateOne(deatils, { $set: list });
-    return query;
-  },
-  deleteList: async function (db, id) {
-    const deatils = { _id: new ObjectID(id) };
+  const query = await db.collection(LISTS_COLLECTION).updateOne(deatils, { $set: list });
+  return query;
+}
+export async function deleteList(db, id) {
+  const deatils = { _id: new ObjectID(id) };
 
-    const query = await db.collection(LISTS_COLLECTION).deleteOne(deatils);
-    return query;
-  },
-  getAllUsers: async function (db) {
-    const query = await db.collection(USERS_COLLECTION).find({});
-    return query.toArray();
-  },
-  addUser: async function (db, user) {
-    const query = await db.collection(USERS_COLLECTION).insertOne(user);
-    return query.ops[0];
-  },
-};
+  const query = await db.collection(LISTS_COLLECTION).deleteOne(deatils);
+  return query;
+}
+export async function getAllUsers(db) {
+  const query = await db.collection(USERS_COLLECTION).find({});
+  return query.toArray();
+}
+export async function addUser(db, user) {
+  const query = await db.collection(USERS_COLLECTION).insertOne(user);
+  return query.ops[0];
+}
